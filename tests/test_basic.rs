5787e079cca89d1de1042b8313cc526f162eb82d
Armin Ronacher <armin.ronacher@active-4.com>
2020-02-08 23:00:03 +0100

    Added tests for pool and changed build to &mut self (#3)

--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::thread;
 use std::time::Duration;
 
@@ -29,4 +30,35 @@ fn test_kill() {
         thread::sleep(Duration::from_secs(10));
     });
     handle.kill().unwrap();
+}
+
+#[test]
+fn test_envvar() {
+    let val = procspawn::Builder::new()
+        .env("FOO", "42")
+        .spawn(23, |val| {
+            env::var("FOO").unwrap().parse::<i32>().unwrap() + val
+        })
+        .join()
+        .unwrap();
+    assert_eq!(val, 42 + 23);
+}
+
+#[test]
+fn test_nested() {
+    let five = spawn(5, |x| {
+        println!("1");
+        let x = spawn(x, |y| {
+            println!("2");
+            y
+        })
+        .join()
+        .unwrap();
+        println!("3");
+        x
+    })
+    .join()
+    .unwrap();
+    println!("4");
+    assert_eq!(five, 5);
 }
